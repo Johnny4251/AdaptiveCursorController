4040dc153bc784b7116a6033c15302ea8c7a256f
Johnny <johnangus425@gmail.com>
2024-04-03 22:51:02 -0400

    Updated Controller.py

--- Controller.py
+++ Controller.py
@@ -13,10 +13,13 @@ model.eval()
 cap = cv2.VideoCapture(0)
 
 prev_position = None
-scaling_factor = 15
 
-def draw_keypoints(outputs, frame, prev_position, scaling_factor):
+# Used to increase sensitivity
+SCALING_FACTOR = 15
+
+def draw_keypoints(outputs, frame, prev_position):
     keypoints_indices = [2] 
+
     for i in range(len(outputs[0]['keypoints'])):
         keypoints = outputs[0]['keypoints'][i].cpu().numpy()
         scores = outputs[0]['scores'][i].cpu().numpy()
@@ -25,8 +28,8 @@ def draw_keypoints(outputs, frame, prev_position, scaling_factor):
                 keypoint = keypoints[idx]
                 current_position = (int(keypoint[0]), int(keypoint[1]))
                 if prev_position is not None:
-                    dx = (current_position[0] - prev_position[0]) * scaling_factor
-                    dy = (current_position[1] - prev_position[1]) * (scaling_factor*3) # makes upwards head movement more sensitive
+                    dx = (current_position[0] - prev_position[0]) * SCALING_FACTOR
+                    dy = (current_position[1] - prev_position[1]) * (SCALING_FACTOR*2) # makes upwards head movement more sensitive
                     new_x = pyautogui.position()[0] + dx
                     new_y = pyautogui.position()[1] + dy
                     pyautogui.moveTo(new_x, new_y)
@@ -34,22 +37,20 @@ def draw_keypoints(outputs, frame, prev_position, scaling_factor):
                 cv2.circle(frame, current_position, 5, (0, 255, 0), thickness=-1)
     return prev_position
 
+# Main Loop
 while True:
     ret, frame = cap.read()
-    #frame = cv2.flip(frame, 1)
     if not ret: break
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_tensor = F.to_tensor(frame_rgb).unsqueeze(0).to(device)
 
-    with torch.no_grad():
-        output = model(frame_tensor)
+    with torch.no_grad(): output = model(frame_tensor)
 
-    prev_position = draw_keypoints(output, frame, prev_position, scaling_factor)
+    prev_position = draw_keypoints(output, frame, prev_position)
 
-    cv2.imshow('Frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.imshow('Debug Window', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 cap.release()
 cv2.destroyAllWindows()

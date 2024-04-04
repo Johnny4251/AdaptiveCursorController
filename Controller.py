22dbec4c41a575285515f562e1475a8190f338fc
Johnny <johnangus425@gmail.com>
2024-04-04 19:59:23 -0400

    performance

--- Controller.py
+++ Controller.py
@@ -4,38 +4,43 @@ import pyautogui
 from torchvision.models.detection import keypointrcnn_resnet50_fpn
 import torchvision.transforms.functional as F
 
+
+SCALING_FACTOR = 60 # Sensitivity
+RIGHT_EYE_INDEX = 1
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
 model = keypointrcnn_resnet50_fpn(pretrained=True).to(device)
 model.eval()
-
 cap = cv2.VideoCapture(0)
 
 prev_position = None
 
-# Used to increase sensitivity
-SCALING_FACTOR = 30
 
-def draw_keypoints(outputs, frame, prev_position):
-    keypoints_indices = [1] 
+def draw_keypoints(outputs, frame, prev_position, alpha=0.2):
+    ema_dx = 0
+    ema_dy = 0
+    scores = outputs[0]['scores'].cpu().numpy()
+    keypoints = outputs[0]['keypoints'].cpu().numpy()
 
-    for i in range(len(outputs[0]['keypoints'])):
-        keypoints = outputs[0]['keypoints'][i].cpu().numpy()
-        scores = outputs[0]['scores'][i].cpu().numpy()
-        if scores > 0.90:
-            for idx in keypoints_indices:
-                keypoint = keypoints[idx]
-                current_position = (int(keypoint[0]), int(keypoint[1]))
-                if prev_position is not None:
-                    dx = (current_position[0] - prev_position[0]) * SCALING_FACTOR
-                    dy = (current_position[1] - prev_position[1]) * (SCALING_FACTOR*2) # makes upwards head movement more sensitive
-                    new_x = pyautogui.position()[0] + dx
-                    new_y = pyautogui.position()[1] + dy
-                    pyautogui.moveTo(new_x, new_y)
-                prev_position = current_position
-                cv2.circle(frame, current_position, 5, (0, 255, 0), thickness=-1)
+    high_confidence_indices = scores > 0.90
+    keypoints = keypoints[high_confidence_indices]
+
+    for keypoint in keypoints:
+        current_position = (int(keypoint[RIGHT_EYE_INDEX, 0]), int(keypoint[RIGHT_EYE_INDEX, 1]))
+        if prev_position is not None:
+            dx = (current_position[0] - prev_position[0]) * SCALING_FACTOR
+            dy = (current_position[1] - prev_position[1]) * (SCALING_FACTOR * 2)
+            ema_dx = alpha * dx + (1 - alpha) * ema_dx
+            ema_dy = alpha * dy + (1 - alpha) * ema_dy
+            new_x = pyautogui.position()[0] + ema_dx
+            new_y = pyautogui.position()[1] + ema_dy
+            pyautogui.moveTo(int(new_x), int(new_y))
+        prev_position = current_position
+        cv2.circle(frame, current_position, 5, (0, 255, 0), thickness=-1)
     return prev_position
+
 
 # Main Loop
 while True:
